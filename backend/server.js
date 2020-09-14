7abcdf728e6186cd85181617ecce4018875af974
Max Li <maxzli@gmail.com>
2020-09-14 13:24:42 -0700

    prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,15 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000
 
+var path = require('path');
+
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '../build')));
+// app.get('/*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
+
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
