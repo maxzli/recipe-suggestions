e7e58860ff8db2b3bfc0c5cdc9297bf8b9aa8baa
Max Li <maxzli@gmail.com>
2020-09-14 15:19:20 -0700

    prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const proxy = require('http-proxy-middleware')
 
 module.exports = function(app) {
     // add other server routes to path array
-    app.use(proxy(['/api' ], { target: 'http://localhost:5000' }));
+    app.use(proxy(['/api' ], { target: 'http://localhost:53680' }));
 }
 
 const uri = process.env.ATLAS_URI;
