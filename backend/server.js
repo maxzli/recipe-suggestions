29a2c22ceaf08d60893c289c97a7487d124750ae
Max Li <maxzli@gmail.com>
2020-09-14 13:45:23 -0700

    prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -11,14 +11,14 @@ var path = require('path');
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../build')));
+app.use('/static', express.static(path.join(__dirname, '../build')));
 // app.get('/*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+//   res.sendFile(path.join(__dirname, '../build', 'index.html'));
 // });
 
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
 );
 const connection = mongoose.connection;
 connection.once('open', () => {
