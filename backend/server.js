7a881d1ad6ce58d0fbf584bbc1b0bd48f6f4fb5a
Max Li <maxzli@gmail.com>
2020-09-14 17:19:51 -0700

    prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -21,13 +21,13 @@ connection.once('open', () => {
 // just added
 if (process.env.NODE_ENV === 'production') {
   // Exprees will serve up production assets
-  app.use(express.static('../build'));
-  // Express serve up index.html file if it doesn't recognize route
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
+  app.use(express.static('../build'))};
+//   // Express serve up index.html file if it doesn't recognize route
+//   const path = require('path');
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//   });
+// }
 
 const ingredientsRouter = require('./routes/ingredients');
 const recipesRouter = require('./routes/recipes');
@@ -36,6 +36,15 @@ const foodsRouter = require('./routes/foods')
 app.use('/ingredients', ingredientsRouter);
 app.use('/recipes', recipesRouter);
 app.use('/foods', foodsRouter);
+
+const router = require('express').Router();
+let Ingredient = require('/models/ingredient.model');
+
+router.route('/').get((req, res) => {
+    Ingredient.find()
+        .then(ingredient => res.json(ingredient))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
